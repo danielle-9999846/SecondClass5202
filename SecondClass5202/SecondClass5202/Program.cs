65e3b70564f962ab6553c6385eee95a03985e16d
Danielle Kumeroa <9999846@student.toiohomai.ac.nz>
2019-07-25 13:29:44 +1200

    SecondClass5202

---
+++ SecondClass5202/SecondClass5202/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondClass5202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter your user name: ");
+            string name = Console.ReadLine();
+            Console.Write("please enter your address: ");
+            string address = Console.ReadLine();
+            Console.Write("please enter your age:");
+            double age = double.Parse(Console.ReadLine());
+            Console.Write("please enter your email address:");
+            string email = Console.ReadLine();
+            Console.Write(" please enter your 4 digit pin:");
+            int pin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n...Details...\nName: {name}\nAddress: {address}\nAge: {age}\nEmail: {email}\nPin: {pin}");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
